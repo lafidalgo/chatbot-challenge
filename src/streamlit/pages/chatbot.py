b0d3f108f46762967d3adef1c1041b6bb0626eaf
Luiz Fidalgo <fidalgo.la@gmail.com>
2024-06-29 15:59:09 -0300

    🐛 Fix bug session state not saving assistant responses

--- src/streamlit/pages/chatbot.py
+++ src/streamlit/pages/chatbot.py
@@ -45,11 +45,13 @@ if prompt := st.chat_input("Envie sua pergunta aqui..."):
     # Display assistant response in chat message container
     with st.chat_message("assistant", avatar=ASSISTANT_AVATAR):
         if st.session_state.check_openai_key:
-            response = st.write(utils.send_question_to_openai_api(prompt))
+            response = utils.send_question_to_openai_api(prompt)
+            st.write(response)
             # response = st.write_stream(
             #    utils.send_question_to_openai_api(prompt, stream=True))
         else:
-            response = st.write_stream(chatbot_funcs.response_generator())
+            response = chatbot_funcs.response_generator()
+            st.write_stream(response)
     # Add assistant response to chat history
     st.session_state.messages.append(
         {"role": "assistant", "avatar": ASSISTANT_AVATAR, "content": response})
